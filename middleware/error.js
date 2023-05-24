64f77488e0eac733c7cc2e24e46e07f5e3909788
SumitEnG <kumarsukdev404@gmail.com>
2023-05-25 00:41:21 +0530

    new

--- middleware/error.js
+++ middleware/error.js
@@ -1,13 +1,13 @@
-const winston = require("winston");
+// const winston = require("winston");
 
 function error(err, req, res, next) {
-  winston.log({
-    level: "error",
-    message: err.message,
-    stack: err.stack,
-    metadata: err, // Put what you like as meta
-  });
-  res.status(500).send("something failed");
+  //   winston.log({
+  //     level: "error",
+  //     message: err.message,
+  //     stack: err.stack,
+  //     metadata: err, // Put what you like as meta
+  //   });
+  res.status(500).send(err);
 }
 
 module.exports = error;
